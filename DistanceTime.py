f828c360b22f83c1eb63b44e8ef7ca97a656db43
rjsuabkghyf2514 <125882977+rjsuabkghyf2514@users.noreply.github.com>
2023-04-18 13:20:57 +0300

    Create DistanceTime.py

---
+++ DistanceTime.py
@@ -0,0 +1,4 @@
+initial_speed = float(input("Введите начальную скорость в м/с: "))
+time = float(input("Введите время в секундах: "))
+acceleration = float(input("Введите ускорение в м/с^2: "))
+distance = initial_speed *
